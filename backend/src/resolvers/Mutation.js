9562add309ffe4f3423eb0c02f39abe94deb8175
Y <y.n.dorny@gmail.com>
2019-04-24 21:05:01 +0200

    Creating Item - 3 : creating a mutation
    from the resolvers/Mutations.js, I call createItem from mutation which is present in my prisma.graphql file and brought through my defined ctx (db.js)

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,13 +1,13 @@
 const mutations = {
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || []
-  //   // create a dog
-  //   const newDog = {
-  //     name: args.name
-  //   }
-  //   global.dogs.push(newDog);
-  //   console.log(args)
-  //   return newDog
-  // }
+
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    return item;
+  }
 };
 module.exports = mutations;
