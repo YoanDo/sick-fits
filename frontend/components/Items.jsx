716110e3cb7b75007e8f576a0c7104f56aab89a1
Y <y.n.dorny@gmail.com>
2019-05-10 14:39:17 +0200

    React meets Graphql - setting up

---
+++ frontend/components/Items.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+class Items extends Component {
+  render() {
+    return (
+      <div>
+        <p>I T E M S</p>
+      </div>
+    );
+  }
+}
+
+export default Items;
