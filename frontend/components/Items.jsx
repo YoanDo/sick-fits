6e0c1af2708b96629a90cb8f5f55e22dc2a4eeef
Y <y.n.dorny@gmail.com>
2019-05-13 11:36:10 +0200

    React meets Graphql - create and bring item

--- frontend/components/Items.jsx
+++ frontend/components/Items.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,19 +17,34 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`
+
 class Items extends Component {
   render() {
     return (
       <div>
-        <p>I T E M S</p>
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data, loading, error }) => {
-            console.log(data, loading, error);
-            if (loading) return <p>Loading</p>
-            if (error) return <p>Error {error.message}</p>
-            return <p>Hey I'm the child of query. <br />look at that {data.items[0].title}</p>;
-          }}
-        </Query>
+        <Center>
+          <Query query={ALL_ITEMS_QUERY}>
+            {({ data, loading, error }) => {
+              console.log(data, loading, error);
+              if (loading) return <p>Loading</p>
+              if (error) return <p>Error {error.message}</p>
+              return <ItemsList>
+                {data.items.map(item => <Item item={item} key={item.id} />)}
+              </ItemsList>;
+            }}
+          </Query>
+        </Center>
       </div>
     );
   }
