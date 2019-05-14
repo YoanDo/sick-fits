c31932786527b6d34b961e0ddf01aa987d735426
Y <y.n.dorny@gmail.com>
2019-05-14 10:01:56 +0200

    Creating Items with Mutations - mutation happen + redirection to item/id

--- frontend/components/CreateItem.jsx
+++ frontend/components/CreateItem.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
-import formatMoney from '../lib/formatMoney'
+import formatMoney from '../lib/formatMoney';
+// display error message 👇
+import ErrorMessage from './ErrorMessage';
 
 const CREATE_ITEM_MUTATION = gql`
 mutation CREATE_ITEM_MUTATION(
@@ -36,37 +39,48 @@ class CreateItem extends Component {
   handleChange = (e) => {
     const { name, type, value } = e.target;
     // console.log({ name: name, type: type, value: value })
-    // As the input value will be inported as a text, we need to change it when the type will be number
+    // As the input value will be imported as a text, we need to change it when the type will be number
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val })
   }
 
   render() {
     return (
-      <Form onSubmit={(e) => {
-        e.preventDefault();
-        console.log(this.state)
-      }}>
-        <h2>Sell an Item.</h2>
-        <fieldset>
-          <label htmlFor="title">
-            Title
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form onSubmit={async e => {
+            //Prevent the form from submitting
+            e.preventDefault();
+            //call the mutation
+            const res = await createItem();
+            //change them to the single item page
+            console.log(res)
+            Router.push({
+              pathname: '/item',
+              query: { id: res.data.createItem.id }
+            })
+          }}>
+            <ErrorMessage error={error} />
+            {/* the bellow disabled will only works if loading is true and prevent the user from using the fieldset */}
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
             <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange} />
-          </label>
+              </label>
 
-          <label htmlFor="price">
-            Price
+              <label htmlFor="price">
+                Price
             <input type="number" id="price" name="price" placeholder="Price" required value={this.state.price} onChange={this.handleChange} />
-          </label>
+              </label>
 
-          <label htmlFor="description">
-            Description
+              <label htmlFor="description">
+                Description
             <textarea type="text" id="description" name="description" placeholder="Description" required value={this.state.description} onChange={this.handleChange} />
-          </label>
-          <button type="submit">Submit</button>
-        </fieldset>
-
-      </Form>
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}</Mutation>
     );
   }
 }
