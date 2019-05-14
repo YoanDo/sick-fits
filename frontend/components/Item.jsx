55851d848ca006b296edd042059653ece1b95105
Y <y.n.dorny@gmail.com>
2019-05-14 16:45:56 +0200

    Deleting items - frontend

--- frontend/components/Item.jsx
+++ frontend/components/Item.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem'
 import formatMoney from '../lib/formatMoney'
 
 class Item extends Component {
@@ -36,7 +37,7 @@ class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add to cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete</DeleteItem>
         </div>
       </ItemStyles >
     );
