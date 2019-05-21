bbd631501edef6668da3c5f272704a3daeb995b9
Y <y.n.dorny@gmail.com>
2019-05-21 17:34:36 +0200

    Currently logged in user - backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,6 @@
-// starts node server
-
-//
-const cookieParser =  require('cookie-parser');
+// this file starts node server
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 // make our variable accessible
 require('dotenv').config({
@@ -17,8 +16,21 @@ const server = createServer();
 
 // use express middleware to handle cookies (json web token)
 server.express.use(cookieParser());
+
 // TODO use express middleware to populate current user
 
+// decode the jwt to get the user ID on each request
+server.express.use((req, res, next) => {
+  const token = req.cookies;
+  if (token) {
+    const {
+      userId
+    } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -5,11 +5,18 @@ const {
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
+  itemsConnection: forwardTo('db'),
+  me(parent, args, info) {
+    // check if there is a current user od
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({
+      where: {
+        id: ctx.request.userId
+      }
+    }, info)
+  }
 };
 
 module.exports = Query
