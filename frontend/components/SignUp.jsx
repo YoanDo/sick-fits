fb3767c81c791a7993f993821024307ce98aafbf
Y <y.n.dorny@gmail.com>
2019-05-21 15:24:04 +0200

    User signup - connection to backend

--- frontend/components/SignUp.jsx
+++ frontend/components/SignUp.jsx
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
-import ErrorMessage from './ErrorMessage'
+import ErrorMessage from './ErrorMessage';
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!,
+    $name: String!,
+    $password: String!
+  ){
+    signup(
+      email: $email,
+      name: $name,
+      password: $password
+    ){
+      id
+      email
+      name
+    }
+  }
+
+`
 
 class SignUp extends Component {
   state = {
@@ -20,36 +39,54 @@ class SignUp extends Component {
   render() {
     const { email, name, password } = this.state
     return (
-      <Form>
-        <fieldset>
-          <h2>Sign up for an account</h2>
-          <label htmlFor="email">
-            email
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              value={email}
-              onChange={this.saveToState}
-            />
-          </label>
-          <label htmlFor="name">
-            name
-            <input
-              type="text"
-              name="name"
-              placeholder="name" value={name} onChange={this.saveToState} />
-          </label>
-          <label htmlFor="password">
-            password
-            <input
-              type="password"
-              name="password"
-              placeholder="password" value={password} onChange={this.saveToState} />
-          </label>
-          <button type="submit">Sign Up!</button>
-        </fieldset>
-      </Form>
+      <Mutation
+        mutation={SIGNUP_MUTATION}
+        variables={this.state}>{(signup, { error, loading }) => {
+          return (
+            <Form method="post" onSubmit={async e => {
+              e.preventDefault();
+              const res = await signup();
+              console.log(res);
+              this.setState({
+                name: '',
+                email: '',
+                password: ''
+              })
+            }}>
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Sign up for an account</h2>
+                <ErrorMessage error={error} />
+                <label htmlFor="email">
+                  email
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor="name">
+                  name
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="name" value={name} onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor="password">
+                  password
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password" value={password} onChange={this.saveToState}
+                  />
+                </label>
+                <button type="submit">Sign Up!</button>
+              </fieldset>
+            </Form>
+          )
+        }}</Mutation>
     );
   }
 }
--- frontend/pages/signup.jsx
+++ frontend/pages/signup.jsx
@@ -5,8 +5,7 @@ const Columns = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
-
-`
+`;
 
 const SignupPage = props => (
   <Columns>
