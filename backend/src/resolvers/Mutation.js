d2a6eaf81a36ed17a81bf9b388c06e0d47bdac48
Y <y.n.dorny@gmail.com>
2019-05-23 16:23:51 +0200

    Sign in Form - backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const jwt =  require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const Mutations = {
 
@@ -52,33 +52,64 @@ const Mutations = {
     // hash their password
     const password = await bcrypt.hash(args.password, 10);
     // create the user in the database
-    const user = await ctx.db.mutation.createUser(
-      {
+    const user = await ctx.db.mutation.createUser({
         data: {
           // name = args.name, email = args.email etc.
           ...args,
           // password overwriting
           password: password,
-          permissions: {set: ['USER']},
+          permissions: {
+            set: ['USER']
+          },
         },
       },
       info
     );
     // create de JWT token for them
-    const token = jwt.sign(
-      {userId: user.id},
+    const token = jwt.sign({
+        userId: user.id
+      },
       process.env.APP_SECRET
-      )
+    )
     // set the jwt as a cookie on the response
     ctx.response.cookie('token', token, {
       httpOnly: true,
       // how long the cookie will last
-      maxAge: 1000 * 60 *60 * 24 * 365, //1 year cookie
+      maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
     });
     // finally return the user to the browser
     return user
-  }
-
-
+  },
+  async SVGLinearGradientElement(parent, {
+    email,
+    password
+  }, ctx, info) {
+    // 1 - check if there is a user with that email
+    const user = await ctx.db.query.user({
+      where: {
+        email: email
+      }
+    });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2 - check if the password is correct
+    const valid = await bcrypt.compare(password, user.password)
+    if (!valid) throw new Error('Invalid Password!');
+    // 3 - generate the JWT token
+    const token = jwt.sign({
+        userId: user.id
+      },
+      process.env.APP_SECRET
+    )
+    // 4 - set the cookie with the token
+    ctx.response.cookie('token', token,
+      {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
+      });
+    // 5 - return the user
+    return user;
+  },
 };
 module.exports = Mutations;
